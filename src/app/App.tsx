f61b9eb684d095e4ad52521e1c841db14db2ae85
Bobur98 <bmuhibullaev@mail.ru>
2024-03-29 01:46:28 +0900

    feat: develop header and footer part one

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,10 +7,9 @@ import { ProductPage } from './screens/productsPage';
 import { HomeNavbar } from './components/headers/HomeNavbar';
 import { OtherNavbar } from './components/headers/OtherNavbar';
 import { Footer } from './components/footer';
-
-// Questions:
-// Why we need reportWebVitals();?
-// Is it good use case to open two terminal and use one of them to run the project, the another one to commit changes?
+import '../css/app.css';
+import '../css/navbar.css';
+import { HelpPage } from './screens/helpPage';
 
 function App() {
     const location = useLocation();
@@ -28,6 +27,9 @@ function App() {
                 </Route>
                 <Route path="/member-page">
                     <UserPage />
+                </Route>
+                <Route path="/help">
+                    <HelpPage />
                 </Route>
                 <Route path="/">
                     <HomePage />
