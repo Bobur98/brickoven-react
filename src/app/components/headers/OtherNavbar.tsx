94debaeb8b3950e50fde50901f92e889d3c681c2
Bobur98 <bmuhibullaev@mail.ru>
2024-03-29 03:04:10 +0900

    feat: publish other navbar header component

--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,3 +1,101 @@
+import { Container, Stack, Box, Button } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
 export function OtherNavbar() {
-    return <div>Other Navbar</div>;
+    const authMember = null;
+
+    return (
+        <div className="other-navbar">
+            <Container className="navbar-container">
+                <Stack className="menu">
+                    <Box className={'hoover-line'}>
+                        <NavLink to="/">
+                            <img
+                                className="brand-logo"
+                                src="/icons/burak.svg"
+                            />
+                        </NavLink>
+                    </Box>
+                    <Stack className="links">
+                        <Box className={'hoover-line'}>
+                            <NavLink to="/" >
+                                Home
+                            </NavLink>
+                        </Box>
+                        <Box className={'hoover-line'}>
+                            <NavLink to="/products" activeClassName="underline">
+                                Products
+                            </NavLink>
+                        </Box>
+                        {authMember ? (
+                            <Box className={'hoover-line'}>
+                                <NavLink
+                                    to="/orders"
+                                    activeClassName="underline"
+                                >
+                                    Orders Page
+                                </NavLink>
+                            </Box>
+                        ) : null}
+                        {authMember ? (
+                            <Box className={'hoover-line'}>
+                                <NavLink
+                                    to="/member-page"
+                                    activeClassName="underline"
+                                >
+                                    My Page
+                                </NavLink>
+                            </Box>
+                        ) : null}
+                        <Box className={'hoover-line'}>
+                            <NavLink to="/help" activeClassName="underline">
+                                Help Page
+                            </NavLink>
+                        </Box>
+                        {/* BASKET */}
+                        {!authMember ? (
+                            <Box>
+                                <Button
+                                    variant="contained"
+                                    className="login-button"
+                                >
+                                    Login
+                                </Button>
+                            </Box>
+                        ) : (
+                            <img
+                                className="user-avatar"
+                                src={'/icons/default-user.svg'}
+                                aria-haspopup={'true'}
+                            />
+                        )}
+                    </Stack>
+                </Stack>
+                <Stack className="header-frame">
+                    <Stack className="detail">
+                        <Box className="head-main-txt">
+                            World's Most Delicious Cousine
+                        </Box>
+                        <Box className="welcome-txt">
+                            The Choice, not just a choice
+                        </Box>
+                        <Box className="service-txt">24 hours service</Box>
+                        <Box className="signup">
+                            {!authMember ? (
+                                <Button
+                                    variant="contained"
+                                    className="signup-button"
+                                >
+                                    SIGN UP
+                                </Button>
+                            ) : null}
+                        </Box>
+                    </Stack>
+                    <Stack className="logo-frame">
+                        <div className="logo-image"></div>
+                    </Stack>
+                </Stack>
+            </Container>
+        </div>
+    );
 }
