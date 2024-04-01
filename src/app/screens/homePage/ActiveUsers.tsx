3e83004572b0d898b4c39863e1d026fe98635116
Bobur98 <bmuhibullaev@mail.ru>
2024-04-01 15:21:42 +0900

    feat: develop activeUsers sectional component

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
 
-import { Container } from '@mui/material';
+import { Box, Container, Stack } from '@mui/material';
+import Card from '@mui/joy/Card';
+import { CssVarsProvider, Typography } from '@mui/joy';
+import { CardOverflow, AspectRatio } from '@mui/joy';
 
 export default function ActiveUsers() {
-    return (
-        <div>
-            <Container>ActiveUsers</Container>
-        </div>
-    );
+  const activeUsers = [
+    { usertName: 'Martin', imagePath: '/img/martin.webp' },
+    { usertName: 'Justin', imagePath: '/img/justin.webp' },
+    { usertName: 'Rose', imagePath: '/img/rose.webp' },
+    { usertName: 'Nusret', imagePath: '/img/nusret.webp' },
+  ];
+
+  return (
+    <div className="active-users-frame">
+      <Container>
+        <Stack className="main">
+          <Box className="category-title">Active Users</Box>
+          <Stack className="cards-frame">
+            <CssVarsProvider>
+              {activeUsers.length !== 0 ? (
+                activeUsers.map((ele, index) => {
+                  return (
+                    <Card key={index} variant="outlined" className="card">
+                      <CardOverflow>
+                        <AspectRatio ratio="1">
+                          <img src={ele.imagePath} alt="" />
+                        </AspectRatio>
+                      </CardOverflow>
+
+                      <CardOverflow variant="soft" className="member-nickname">
+                        <Typography>{ele.usertName}</Typography>
+                      </CardOverflow>
+                    </Card>
+                  );
+                })
+              ) : (
+                <Box className="no-data">No Active Users!</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
