bfd7108d81aa67a8549ea3f9a6689dc4fa838db0
Bobur98 <bmuhibullaev@mail.ru>
2024-04-02 19:26:46 +0900

    feat: develop productPage nested routing system

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,3 +1,22 @@
-export default function ProductPage() {
-    return <h2>ProductPage</h2>;
+import { Route, Router, Switch, useRouteMatch } from 'react-router-dom';
+import ChosenProduct from './ChosenProduct';
+import Products from './Products';
+
+export default function ProductsPage() {
+  //productsdan qaytyabkan path bn urlni nima farqi bor?
+  const products = useRouteMatch();
+  console.log(products);
+
+  return (
+    <div className="products-page">
+      <Switch>
+        <Route path={`${products.path}/:productId`}>
+          <ChosenProduct />
+        </Route>
+        <Route path={`${products.path}`}>
+          <Products />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
