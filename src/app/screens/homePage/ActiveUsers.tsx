993775ba77374e53623a9bd3637eeaaefd28c671
Bobur98 <bmuhibullaev@mail.ru>
2024-04-24 16:51:33 +0900

    fix: retreive top users and modify activeUsers component

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -4,14 +4,18 @@ import { Box, Container, Stack } from '@mui/material';
 import Card from '@mui/joy/Card';
 import { CssVarsProvider, Typography } from '@mui/joy';
 import { CardOverflow, AspectRatio } from '@mui/joy';
+import { createSelector } from '@reduxjs/toolkit';
+import { retrieveTopUsers } from './selector';
+import { useSelector } from 'react-redux';
+import { serverApi } from '../../../lib/config';
+import { Member } from '../../../lib/types/member';
+
+const retreiveTopUsers = createSelector(retrieveTopUsers, (topUsers) => ({
+  topUsers,
+}));
 
 export default function ActiveUsers() {
-  const activeUsers = [
-    { usertName: 'Martin', imagePath: '/img/martin.webp' },
-    { usertName: 'Justin', imagePath: '/img/justin.webp' },
-    { usertName: 'Rose', imagePath: '/img/rose.webp' },
-    { usertName: 'Nusret', imagePath: '/img/nusret.webp' },
-  ];
+  const { topUsers } = useSelector(retreiveTopUsers);
 
   return (
     <div className="active-users-frame">
@@ -20,18 +24,19 @@ export default function ActiveUsers() {
           <Box className="category-title">Active Users</Box>
           <Stack className="cards-frame">
             <CssVarsProvider>
-              {activeUsers.length !== 0 ? (
-                activeUsers.map((ele, index) => {
+              {topUsers.length !== 0 ? (
+                topUsers.map((member: Member) => {
+                  const imagePath = `${serverApi}/${member.memberImage}`;
                   return (
-                    <Card key={index} variant="outlined" className="card">
+                    <Card key={member._id} variant="outlined" className="card">
                       <CardOverflow>
                         <AspectRatio ratio="1">
-                          <img src={ele.imagePath} alt="" />
+                          <img src={imagePath} alt="" />
                         </AspectRatio>
                       </CardOverflow>
 
                       <CardOverflow variant="soft" className="member-nickname">
-                        <Typography>{ele.usertName}</Typography>
+                        <Typography>{member.memberNick}</Typography>
                       </CardOverflow>
                     </Card>
                   );
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -8,28 +8,33 @@ import Statistics from './Statistics';
 import '../../../css/home.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { Dispatch } from '@reduxjs/toolkit';
-import { setNewDishes, setPopularDishes } from './slice';
+import { setNewDishes, setPopularDishes, setTopUsers } from './slice';
 import { Product } from '../../../lib/types/products';
 import ProductService from '../../services/ProductService';
 import { ProductCollection } from '../../../lib/enums/product.enum';
+import MemberService from '../../services/MemberService';
+import { Member } from '../../../lib/types/member';
 
 /** REDUX SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
   // 2. setPopularDishes reducer ikta argument qabul qiladi ln biz bita parameter jonatyabmiz?
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
   setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
+  setTopUsers: (data: Member[]) => dispatch(setTopUsers(data)),
 });
 
 export default function HomePage() {
   // 3. useDispatch() actionDispatchdagi dispatch argumentmi?
   // Selector: Store => Data
-  const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch());
+  const { setPopularDishes, setNewDishes, setTopUsers } = actionDispatch(
+    useDispatch()
+  );
   console.log(process.env.REACT_APP_API_URL);
 
   useEffect(() => {
     // Backend server data request => Data
     const products = new ProductService();
-
+    const members = new MemberService();
     products
       .getProducts({
         order: 'productViews',
@@ -37,12 +42,8 @@ export default function HomePage() {
         limit: 4,
         productCollection: ProductCollection.DRINK,
       })
-      .then((data) => {
-        setPopularDishes(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      .then((data) => setPopularDishes(data))
+      .catch((err) => console.log(err));
 
     products
       .getProducts({
@@ -50,12 +51,13 @@ export default function HomePage() {
         page: 1,
         limit: 4,
       })
-      .then((data) => {
-        setNewDishes(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      .then((data) => setNewDishes(data))
+      .catch((err) => console.log(err));
+
+    members
+      .getTopUsers()
+      .then((data) => setTopUsers(data))
+      .catch((err) => console.log(err));
 
     // Slice: Data => Store
     // @ts-ignore
