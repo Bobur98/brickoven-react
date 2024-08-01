b4198cdc5035817fd6f8bd1ca639a88b08cc0ba7
Bobur98 <bmuhibullaev@mail.ru>
2024-08-02 02:10:03 +0900

    feat: deploy  project via shell and pm2

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# PRODUCTION
+git reset --hard
+git checkout master
+git pull origin master
+
+npm i yarn -g
+yarn global add serve
+yarn
+yarn run build
+pm2 start yarn --name BRICKOVEN-REACT -- prod
+
+# DEVELOPMENT
+# git reset --hard
+# git checkout develop
+# git pull origin develop
+
+# npm i
+# pm2 start "npm run start:dev" --name=BRICKOVEN
