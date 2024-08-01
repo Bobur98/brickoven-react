d684cf7d7f83fe0b93c5c998db18d26565790b9c
Bobur98 <bmuhibullaev@mail.ru>
2024-08-02 03:00:24 +0900

    fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # PRODUCTION
-git reset --hard
-git checkout master
-git pull origin master
+# git reset --hard
+# git checkout master
+# git pull origin master
 
 npm i yarn -g
 yarn global add serve
 yarn
 yarn run build
-pm2 start yarn --name BRICKOVEN-REACT -- prod
+pm2 start cmd --name BRICKOVEN-REACT -- /c "yarn prod"
 
 # DEVELOPMENT
 # git reset --hard
